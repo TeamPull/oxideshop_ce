6b3ee7566a4ac0b5f4a1ca3e83c45fa379483a55
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2016-08-13 15:58:36 +0200

    more tests

--- tests/Unit/Application/Controller/ViewConfigTest.php
+++ tests/Unit/Application/Controller/ViewConfigTest.php
@@ -559,9 +559,19 @@ class ViewConfigTest extends \OxidTestCase
         $this->assertEquals("{$baseUrl}modules/test1/out/blocks/test2.tpl", $viewConfig->getModuleUrl('test1', 'out/blocks/test2.tpl'));
         $this->assertEquals("{$baseUrl}modules/test1/out/blocks/test2.tpl", $viewConfig->getModuleUrl('test1', '/out/blocks/test2.tpl'));
         $this->assertEquals("{$baseUrl}modules/test1/out", $viewConfig->getModuleUrl('test1'));
+        
+        //test if the subjevt under test still generates a valid module url in admin mode
         $config->setAdminMode(true);
         $viewConfig->setAdminMode(true);
         print $viewConfig->getModuleUrl('test1');
+        //in oir test environment the domain for admin area is the normal shopurl
+        $adminUrlWithoutAdminPath = $baseUrl;
+        $this->assertEquals("{$adminUrlWithoutAdminPath}modules/test1/out", $viewConfig->getModuleUrl('test1'));
+
+        $config->setConfigParam('sAdminUrl','http://admin.localhost.local/admin');
+        $this->assertEquals("http://admin.localhost.local/modules/test1/out", $viewConfig->getModuleUrl('test1'));
+
+        
     }
 
     public function testGetModuleUrlExceptionThrownWhenPathNotFoundAndDebugEnabled()
