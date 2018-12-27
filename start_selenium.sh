de2b6699cdb55d0994cbbb876cdb7652e59dac1b
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2018-12-27 12:08:20 +0100

    temp. remove some noise

--- start_selenium.sh
+++ start_selenium.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 xfce4-session&
-java -jar selenium-server-standalone-2.47.1.jar&
+java -jar selenium-server-standalone-2.47.1.jar &>/dev/null&
 vendor/bin/runtests-selenium
